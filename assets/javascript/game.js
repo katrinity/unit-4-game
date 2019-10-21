469273af072c026db01820b150d032a8012f2633
K G <katrinity1@yahoo.com>
2019-10-20 20:53:10 -0700

    added JS page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,53 @@
-let NUMBER = Math.floor((Math.random() * 120) + 19);
-let CRYSTALVALUE = Math.floor((Math.random() * 12) + 1);
+let number ;//= Math.floor((Math.random() * 101) + 19);
+let crystalvalue1;// = Math.floor((Math.random() * 11) + 1);
+let crystalvalue2;// = Math.floor((Math.random() * 11) + 1);
+let crystalvalue3;// = Math.floor((Math.random() * 11) + 1);
+let crystalvalue4;// = Math.floor((Math.random() * 11) + 1);
 
-let NUM_WINS = 0;
-let NUM_LOSES = 0;
+let num_wins;// = 0;
+let num_losses;// = 0;
+let score_counter;// = 0;
 
 
 const reset = () => {
-    NUM_WINS = 0;
-    NUM_LOSES = 0;
-    NUMBER = Math.floor((Math.random() * 120) + 19);
-    CRYSTALVALUE = Math.floor((Math.random() * 12) + 1);
+    num_wins = 0;
+    num_losses = 0;
+    score_counter = 0;
+    number = Math.floor((Math.random() * 101) + 19);
+    crystalvalue1 = Math.floor((Math.random() * 11) + 1);
+    crystalvalue2 = Math.floor((Math.random() * 11) + 1);
+    crystalvalue3 = Math.floor((Math.random() * 11) + 1);
+    crystalvalue4 = Math.floor((Math.random() * 11) + 1);
+
+    $('#wins').html('Wins: ' + num_wins);
+    $('#losses').html(`Losses: ${num_losses}`);
+    $('#randomnumber').html(number);
     
-    $('#wins').html('Wins: ' + NUM_WINS);
-    $('#losses').html(`Losses: ${NUM_LOSES}`);
-    $('#randomnumber').html('' + String.fromCharCode(NUMBER));
 };
 
-reset();
+$(document).ready(() => {
+    reset();
+    $('.button1').html(crystalvalue1);
+    $('.button2').html(crystalvalue2);
+    $('.button3').html(crystalvalue3);
+    $('.button4').html(crystalvalue4);
+    
+    $('.button1').on("click", event => {
+        score_counter+=crystalvalue1;
+        $('#score_counter').html(score_counter);
+    });
+    $('.button2').on("click", event => {
+        score_counter+=crystalvalue2;
+        $('#score_counter').html(score_counter);
+    });
+    $('.button3').on("click", event => {
+        score_counter+=crystalvalue3;
+        $('#score_counter').html(score_counter);
+    });
+    $('.button4').on("click", event => {
+        score_counter+=crystalvalue4;
+        $('#score_counter').html(score_counter);
+    });
+});
+
 
